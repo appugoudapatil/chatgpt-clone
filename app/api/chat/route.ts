419397d59b3e7ab0979831bf7f89987167b32586
pp123 <pp123@Anupamas-MacBook-Pro.local>
2024-01-09 01:05:25 +0530

    fix-api path

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -3,20 +3,22 @@
 import { OpenAIStream, StreamingTextResponse } from "ai";
 import { Configuration, OpenAIApi } from "openai-edge";
 
+// IMPORTANT! Set the runtime to edge
+export const runtime = "edge";
+
 // Create an OpenAI API client (that's edge friendly!)
 const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
-const openai = new OpenAIApi(config);
 
-// IMPORTANT! Set the runtime to edge
-export const runtime = "edge";
+console.log("vapiKey", config.apiKey);
+const openai = new OpenAIApi(config);
 
 export async function POST(req: Request) {
   // Extract the `messages` from the body of the request
-  const { messages } = await req.json();
+  const body = await req.json();
+  const { messages } = body;
 
-  console.log("MESSAGES : ", messages);
   // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,37 @@
 "use client";
 
+import GPTLogo from "@components/GPTLogo";
 import { useChat } from "ai/react";
 
 export default function Home() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
 
   return (
-    <div className="pt-4 pb-32">
+    <div className="pt-4 pb-32  bg-white ml-20 mr-20 pl-9 pr-9">
       {messages.map((m) => (
-        <div key={m.id} className="border-t border-black/10 ">
+        <div
+          key={m.id}
+          className={`border-t border-black/10 ${
+            m.role === "assistant" && "bg-gray-100"
+          }`}
+        >
           <div className="max-w-3x1 mx-auto py-6 flex">
-            <span className="ml-3">
-              {m.role}: {m.content}
-            </span>
+            {m.role === "assistant" && <GPTLogo />}
+            <span className="ml-3  text-black">{m.content}</span>
           </div>
         </div>
       ))}
 
-      <form onSubmit={handleSubmit} className="fixed inset-x-0 bottom-10">
+      <form
+        onSubmit={handleSubmit}
+        className="pt-4 fixed inset-x-0 bottom-10 ml-20 mr-20 pl-9 pr-9"
+      >
         <label>
           <input
-            className="max-w-3x1 shadow-xl w-full mx-auto py-8 flex h-10 rounded-md border border-input bg-background px-3 text-sm ring-offset-background"
+            className="max-w-3x1 shadow-xl w-full mx-auto py-8 flex h-10 rounded-md border border-input bg-background px-3 text-sm ring-offset-background text-black"
             value={input}
             onChange={handleInputChange}
-            placeholder="Enter a prompt"
+            placeholder="Message CloudGPT..."
           />
         </label>
       </form>
